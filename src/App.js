9af308cd8c905ea9fa3c3c388aeea820cb38f5aa
DatsSameBoi <142678486+DatsSameBoi@users.noreply.github.com>
2023-08-19 18:03:14 +0100

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import yemi from 'yemi.js';
 
 export default function App() {
   return (
-    <div>
-      <h1>hello yemi</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <center>
+      <h1>Yemi</h1>
+      <a href="Index2.html"> Home</a> |<a href="Pictures"> Pictures </a> |
+      <a href="Musix"> Musix </a> |<a href="Videoz"> Videoz </a> |
+      <a href="About me"> About Me </a> |
+      <a href="Contacts.html"> Contact us </a> |
+      <a href="Terms of service"> Terms of service </a> |
+      <a href="Privacy policy"> Privacy policy </a> |
+      <a href="company/about.html">About me</a> |
+      <a href="Yemi.jpg"> My Photo"</a> |<a href="#section-html"> HTML</a> |
+      <a href="#section-css"> CSS</a> |
+      <a href="https://google.com" target="_blank">
+        Google
+      </a>
+      <a href="mailto:datsameboi@gmail.com"> Email Me</a> |
+      <a href="Image Allign.html"> index</a> |
+      <a href="Bracket ws.html"> Bracket Project</a> |
+      <a href="Learning HTML.html"> Learning HTML</a> |
+    </center>
   );
 }
