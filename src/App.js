2514ac9d9d1dcb2bf00394f6d81e023076c4c2b5
DatsSameBoi <142678486+DatsSameBoi@users.noreply.github.com>
2023-08-25 13:53:12 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import yemi from 'yemi.js';
 
 export default function App() {
   return (
-    <center>
-      <h1>Yemi</h1>
-      <a href="Index2.html"> Home</a> |<a href="Pictures"> Pictures </a> |
-      <a href="Musix"> Musix </a> |<a href="Videoz"> Videoz </a> |
-      <a href="About me"> About Me </a> |
-      <a href="Contacts.html"> Contact us </a> |
-      <a href="Terms of service"> Terms of service </a> |
-      <a href="Privacy policy"> Privacy policy </a> |
-      <a href="company/about.html">About me</a> |
-      <a href="Yemi.jpg"> My Photo"</a> |<a href="#section-html"> HTML</a> |
-      <a href="#section-css"> CSS</a> |
-      <a href="https://google.com" target="_blank">
-        Google
-      </a>
-      <a href="mailto:datsameboi@gmail.com"> Email Me</a> |
-      <a href="Image Allign.html"> index</a> |
-      <a href="Bracket ws.html"> Bracket Project</a> |
-      <a href="Learning HTML.html"> Learning HTML</a> |
-    </center>
+    <body bgcolor="silver">
+      <center>
+        <h1>Yemi</h1>
+        <a href="Index2.html"> Home</a> |<a href="Pictures"> Pictures </a> |
+        <a href="Musix"> Musix </a> |<a href="Videoz"> Videoz </a> |
+        <a href="About me"> About Me </a> |
+        <a href="Contacts.html"> Contact us </a> |
+        <a href="Terms of service"> Terms of service </a> |
+        <a href="Privacy policy"> Privacy policy </a> |
+        <a href="company/about.html">About me</a> |
+        <a href="Yemi.jpg"> My Photo"</a> |<a href="#section-html"> HTML</a> |
+        <a href="#section-css"> CSS</a> |
+        <a href="https://google.com" target="_blank">
+          Google
+        </a>
+        <a href="mailto:datsameboi@gmail.com"> Email Me</a> |
+        <a href="Image Allign.html"> index</a> |
+        <a href="Bracket ws.html"> Bracket Project</a> |
+        <a href="Learning HTML.html"> Learning HTML</a> |
+      </center>
+    </body>
   );
 }
